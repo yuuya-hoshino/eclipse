725476cfcf57cb3c614161d9c4ae65b522072f66
yuuya-hoshino <sjlthmfjx@icloud.com>
2023-06-09 12:17:25 +0900

    Code5_2を追加

---
+++ tesu11/src/tesu11/Code5_2.java
@@ -0,0 +1,15 @@
+package tesu11;
+
+public class Code5_2 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		System.out.println("メソッドを呼び出します");
+		hello();
+		System.out.println("メソッドの呼び出しが終わりました");
+	}
+	public static void hello() {
+		System.out.println("湊さん、こんにちは");
+	}
+
+}
