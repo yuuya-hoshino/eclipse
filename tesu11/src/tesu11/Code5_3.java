16d5cf927609b5e7495613122b17a81de496aaf7
yuuya-hoshino <sjlthmfjx@icloud.com>
2023-06-09 12:39:05 +0900

    メソッドからメソッドの受け渡し

---
+++ tesu11/src/tesu11/Code5_3.java
@@ -0,0 +1,17 @@
+package tesu11;
+
+public class Code5_3 {
+	public static void methodA() {
+		System.out.println("methodA");
+		methodB();
+	}
+	public static void methodB() {
+		System.out.println("methodB");
+	}
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		methodA();
+	}
+
+}
+
