ad57993efdc21b78f6b70f0f7f2ec38581eb9adf
yuuya-hoshino <sjlthmfjx@icloud.com>
2023-06-15 11:04:51 +0900

    5_5追加

--- tesu11/src/tesu11/Code5_5.java
+++ tesu11/src/tesu11/Code5_5.java
@@ -4,7 +4,17 @@ public class Code5_5 {
 
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
-		
+		add(100,20);
+		add(200,50);
+	}
+	//メソッド名:add(加算)
+	//戻り値:なし
+	//引数:int x , int y
+	//処理:引数で貰ったx,yを加算した結果を
+	//x+y=(合計)の形式で出力する
+	public static void add(int x,int y) {
+		int ans = x + y;
+		System.out.println(x + "+" + y + "=" + ans);
 	}
 
 }
